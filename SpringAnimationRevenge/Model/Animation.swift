f1c1b7364f61bfaaca3a66ac1fd01ca3b8ce616a
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-12-10 18:18:49 +0700

    Running animation

--- SpringAnimationRevenge/Model/Animation.swift
+++ SpringAnimationRevenge/Model/Animation.swift
@@ -9,9 +9,28 @@ import Foundation
 import SpringAnimation
 
 struct Animation {
-    let preset: AnimationPreset
-    let curve: AnimationCurve
+    let preset: String
+    let curve: String
     let force: CGFloat
     let duration: CGFloat
     let delay: CGFloat
+    var animationDescription: String {
+        """
+        preset : \(self.preset)
+        curve : \(self.curve)
+        force : \(self.force)
+        duration : \(self.duration)
+        delay : \(self.delay)
+        """
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        Animation(
+            preset: AnimationPreset.allCases.randomElement()?.rawValue ?? "morph",
+            curve: AnimationCurve.allCases.randomElement()?.rawValue ?? "easeIn",
+            force: CGFloat.random(in: 0.5...10),
+            duration: CGFloat.random(in: 0.5...4),
+            delay: CGFloat.random(in: 0.5...1.5)
+        )
+    }
 }
--- SpringAnimationRevenge/ViewController.swift
+++ SpringAnimationRevenge/ViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    private var currentAnimation = Animation.getRandomAnimation()
     @IBOutlet weak var animatedView: SpringView!
+    @IBOutlet weak var animatedViewLable: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animatedView.layer.cornerRadius = 30
+        animatedViewLable.text = currentAnimation.animationDescription
     }
 
     @IBAction func runAnimationButton(_ sender: SpringButton) {
-        animatedView.animation = "morph"
+        animatedViewLable.text = currentAnimation.animationDescription
+        animatedView.animation = currentAnimation.preset
+        animatedView.curve = currentAnimation.curve
+        animatedView.force = currentAnimation.force
+        animatedView.duration = currentAnimation.duration
+        animatedView.delay = currentAnimation.delay
         animatedView.animate()
+        
+        currentAnimation = Animation.getRandomAnimation()
+        sender.setTitle("Run \(currentAnimation.preset)", for: .normal)
     }
     
 
