9db8fbd4034b6d0c8552285c668d35a3843536c4
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-12-10 17:17:48 +0700

    View and button are created. One animation is checked.

---
+++ SpringAnimationRevenge/Model/Animation.swift
@@ -0,0 +1,17 @@
+//
+//  Animation.swift
+//  SpringAnimationRevenge
+//
+//  Created by Olga Yurchuk on 08.12.2022.
+//
+
+import Foundation
+import SpringAnimation
+
+struct Animation {
+    let preset: AnimationPreset
+    let curve: AnimationCurve
+    let force: CGFloat
+    let duration: CGFloat
+    let delay: CGFloat
+}
--- SpringAnimationRevenge/ViewController.swift
+++ SpringAnimationRevenge/ViewController.swift
@@ -5,15 +5,24 @@
 //  Created by Olga Yurchuk on 08.12.2022.
 //
 
+import SpringAnimation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var animatedView: SpringView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animatedView.layer.cornerRadius = 30
     }
 
+    @IBAction func runAnimationButton(_ sender: SpringButton) {
+        animatedView.animation = "morph"
+        animatedView.animate()
+    }
+    
 
 }
 
